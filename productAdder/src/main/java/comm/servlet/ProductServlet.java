2b4c10f65664b477b827b3309e0702eaa76e6d4b
ChiluveruSra1 <Chiluveru123@gmail.com>
2020-01-27 17:19:12 +0530

    product adder

---
+++ productAdder/src/main/java/comm/servlet/ProductServlet.java
@@ -0,0 +1,44 @@
+package comm.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		processRequest(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		try {
+			response.setContentType("text/html");
+			
+			PrintWriter out = response.getWriter();
+			out.println(request.getParameter("name"));
+			out.println("hello");
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+}
